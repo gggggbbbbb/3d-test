688532587e4e956070fdc4a7c4dd43a94ea6deab
giuliobrugnaro@gmail.com <giuliobrugnaro@gmail.com>
2019-07-01 11:28:33 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>TEST</h2>
+    <div class="viewer">
+      <model-stl src="assets\gear.stl"></model-stl>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModelStl  from 'vue-3d-model/src/model-stl.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    ModelStl
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'vue-3d-model/dist/vue-3d-model.umd.min.js'
+
 Vue.config.productionTip = false
 
 new Vue({
