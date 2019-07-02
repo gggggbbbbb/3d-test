888310d086780c8f93a8a2ade64d81ff77c93b96
giuliobrugnaro@gmail.com <giuliobrugnaro@gmail.com>
2019-07-02 10:39:26 +0200

    added vue-bootstrap modals

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <h2>TEST</h2>
-    <div class="viewer">
-      <model-stl src="assets\gear.stl"></model-stl>
+
+    <div>
+      <b-button v-b-modal.modal-1 class=mx-4>Open 3D (No size defined)</b-button>
+
+      <b-modal id="modal-1" title="BootstrapVue" hide-footer="true" size="lg" >
+         <model-stl src="static/models/gear.stl"></model-stl>
+      </b-modal>
+
+      <b-button v-b-modal.modal-2 class=mx-4>Open 3D (Size defined)</b-button>
+
+      <b-modal id="modal-2" title="BootstrapVue" hide-footer="true" size="lg">
+         <model-stl width=400 height=400 src="static/models/gear.stl"></model-stl>
+      </b-modal>
     </div>
   </div>
 </template>
@@ -13,6 +23,6 @@ import ModelStl  from 'vue-3d-model/src/model-stl.vue'
 export default {
   components: {
     ModelStl
-  }
+  } 
 }
 </script>
