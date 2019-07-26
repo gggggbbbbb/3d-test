c6d286eaff4cbdd5840785454ac2a8ce5c5c3d1b
giuliobrugnaro@gmail.com <giuliobrugnaro@gmail.com>
2019-07-26 10:40:04 +0200

    26.07.2019

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div>
-
-    <div>
-      <b-button v-b-modal.modal-1 class=mx-4>Open 3D (No size defined)</b-button>
-
-      <b-modal id="modal-1" title="BootstrapVue" hide-footer="true" size="lg" >
-         <model-stl src="static/models/gear.stl"></model-stl>
-      </b-modal>
-
-      <b-button v-b-modal.modal-2 class=mx-4>Open 3D (Size defined)</b-button>
-
-      <b-modal id="modal-2" title="BootstrapVue" hide-footer="true" size="lg">
-         <model-stl width=400 height=400 src="static/models/gear.stl"></model-stl>
-      </b-modal>
-    </div>
-  </div>
+    <div style="height: 550px" class="view" >
+      <model-stl ref="model" src="/static/models/gear.stl"></model-stl>
+    <button @click="takeSnap">Snapshot</button>
+    </div>    
 </template>
 
 <script>
 import ModelStl  from 'vue-3d-model/src/model-stl.vue'
-
 export default {
   components: {
     ModelStl
-  } 
+  } ,
+  data(){
+    return{
+    }
+  },
+
+  methods:{
+      takeSnap(){
+          let {renderer}  = this.$refs.model;
+          let image =  renderer.domElement.toDataURL("image/png");
+          window.open(image); 
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
 
 import 'vue-3d-model/dist/vue-3d-model.umd.min.js'
 
